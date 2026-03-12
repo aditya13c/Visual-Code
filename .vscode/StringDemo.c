9a9f739bcc8f3d4674449322feeb2f8729a31a8f
aditya288 <adityachouhan7016@gmail.com>
2026-03-12 17:57:05 +0530

    StringDemo

---
+++ .vscode/StringDemo.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+int main() {
+    // char name[] = "Adi";
+    // printf("\n%s\n",name);
+
+    // char str[6]={'H','E','L','L','O','\0'};
+    // printf("\n%s\n",str);
+
+    // char username[20];
+    // char actual_username[] = "Adi";
+
+    // printf("Enter your username: ");
+    // scanf("%s", username);
+
+    // if (strcmp(username, actual_username) == 0) 
+    // {
+
+    //     printf("Username is Correct" );
+    
+    // }
+    // else {
+    //     printf("Invalid username. Access denied.\n");
+    // }
+    // printf ("\nlength of username: %lu\n", strlen(actual_username));
+   
+   
+            /* String Copy Fun*/
+
+// char str1[]="adi";
+// char str2[20];
+
+// strcpy(str2,str1);
+// printf("%s\n",str2);
+
+             /* String Reverse Fun  */
+
+// char str1[]="adi";
+// char str2[20];
+
+// strcpy(str2,str1);
+// printf("%s\n",str2);
+
+// strrev(str1);
+// printf("rev is %s\n",str1);
+
+               /* String Concatenation Fun  */
+
+char str []="adi";
+char str2[]="arc";
+
+strcat(str,str2);
+printf("%s\n",str);
+
+    return 0;
+} 
